92707d55a23cc0dfac3b41a43f4fd58f071cf4cb
Thimira Nirmal <timnirmal@gmail.com>
2023-03-15 16:05:30 +0530

    logs added

--- test_0_hyperparamter_tunning.py
+++ test_0_hyperparamter_tunning.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from matplotlib import pyplot
 from prophet import Prophet
-from lib.datetimegen import generate_datetimes as dgt
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from prophet.serialize import model_to_json, model_from_json
 from prophet.diagnostics import cross_validation, performance_metrics
 import itertools
 import numpy as np
+import os
+from prophet.plot import add_changepoints_to_plot
 
 # load every column in pd head
 pd.set_option('display.max_columns', None)
@@ -62,20 +63,47 @@ holidays = playoffs
 cutoffs = pd.to_datetime(['2020-06-01', '2021-06-01', '2022-06-01'])
 
 param_grid = {
-    'changepoint_prior_scale': [0.001, 0.01, 0.1, 0.5, 1],
-    'seasonality_prior_scale': [0.01, 0.1, 1],
-    'holidays': [holidays],
+    'changepoint_prior_scale': [0.001, 0.01, 0.05, 0.1, 0.5],
+    # 'changepoint_prior_scale': [1, 0.5, 0.1, 0.05, 0.01, 0.001],
+    'seasonality_prior_scale': [0.01, 0.1, 1.0, 10.0],
+    # 'seasonality_prior_scale': [100, 10, 1, 0.1, 0.01],
+    'holidays': [holidays, None],
+    # 'holidays': [None, holidays],
     'seasonality_mode': ['multiplicative'],
-    'holidays_prior_scale': [10],
-    'mcmc_samples': [0],
-    'interval_width': [1],
-    'uncertainty_samples': [2000],
+    'holidays_prior_scale': [0.01, 0.1, 1.0, 10.0],
+    # 'holidays_prior_scale': [100, 10, 1, 0.1, 0.01, 0],
+    'mcmc_samples': [0, 10, 100, 1000],
+    # 'mcmc_samples': [1000, 100, 10, 0],
+    'interval_width': [0.5, 0.8, 1],
+    # 'interval_width': [100, 10, 1, 0.5, 0],
+    'uncertainty_samples': [1000, 2000, 5000],
+    # 'uncertainty_samples': [5000, 2000, 1000],
     'stan_backend': [None],
-    'changepoint_range': [1],
-    'yearly_seasonality': [True],
-    'weekly_seasonality': [True],
-    'daily_seasonality': [True],
+    'changepoint_range': [0.8, 0.9, 1],
+    # 'changepoint_range': [1, 0.9, 0.8],
+    'yearly_seasonality': ['auto', True, False],
+    'weekly_seasonality': ['auto', True, False],
+    'daily_seasonality': ['auto', True, False],
+    # 'growth': ['linear', 'logistic'],
+    'growth': ['linear'],
+    'n_changepoints': [25, 50, 100, 200, 500],
+    # 'n_changepoints': [500, 200, 100, 50, 25],
+
 }
+
+# create results folder
+
+if not os.path.exists("results"):
+    os.makedirs("results")
+
+folder_name = "prophet-results-" + str(pd.to_datetime('today').strftime("%Y%m%d-%H%M%S"))
+
+# inside results folder create folder named  now date(YYYYMMDD)-time(HHMMSS)
+if not os.path.exists("results/" + folder_name):
+    os.makedirs("results/" + folder_name)
+
+# create a file named results.txt
+results_file = open("results/" + folder_name + "/results.txt", "w+")
 
 # Generate all combinations of parameters
 all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
@@ -83,16 +111,43 @@ rmses = []  # Store the RMSEs for each params here
 
 # Use cross validation to evaluate all parameters
 for params in all_params:
+    """LOG"""
+    # write iteration number, datetime of run to folder_name/results.txt
+    with open("results/" + folder_name + "/results.txt", "a") as myfile:
+        myfile.write("\n\n============================================================\n")
+        myfile.write("Iteration : " + str(all_params.index(params)) + "\n")
+        myfile.write("Datetime : " + str(pd.to_datetime('today').strftime("%Y%m%d-%H%M%S")) + "\n\n")
+    # print params
+    print("Params: ", params)
+    # for each item in params json, write it to the file
+    with open("results/" + folder_name + "/results.txt", "a") as results_file:
+        results_file.write("Params: \n")
+        for key, value in params.items():
+            results_file.write("%s: %s\n" % (key, value))
+        results_file.write("\n\n")
+
+    print("\nIterration : ", (all_params.index(params)))
+    print("Model Training...\n")
     m = Prophet(**params).fit(df)  # Fit model with given params
     # df_cv = cross_validation(model, initial='730 days', period='180 days', horizon='365 days')
+
+    print("\nCross Validation...\n")
     df_cv = cross_validation(m, cutoffs=cutoffs, horizon='30 days')
     df_p = performance_metrics(df_cv)
     rmses.append(df_p['rmse'].values[0])
 
+    """LOG"""
+    # save df_cv to results folder with iteration number
+    df_cv.to_csv("results/" + folder_name + "/df_cv-" + str(all_params.index(params)) + ".csv", index=False)
+    # save df_p to results folder
+    df_p.to_csv("results/" + folder_name + "/df_p.csv", index=False)
+
     future = m.make_future_dataframe(periods=200, freq='D')
+    future.to_csv("results/" + folder_name + "/future.csv", index=False)
 
     # use the model to make a forecast
     forecast = m.predict(future)
+    forecast.to_csv("results/" + folder_name + "/forecast.csv", index=False)
 
     r2_score(df["y"], forecast["yhat"][:len(df["y"])])
     mean_squared_error(df["y"], forecast["yhat"][:len(df["y"])])
@@ -103,6 +158,32 @@ for params in all_params:
     print("MSE: ", mean_squared_error(df["y"], forecast["yhat"][:len(df["y"])]))
     print("MAE: ", mean_absolute_error(df["y"], forecast["yhat"][:len(df["y"])]))
 
+    # Metrics to log
+    with open("results/" + folder_name + "/results.txt", "a") as myfile:
+        myfile.write("\n\nMetrics\n")
+        myfile.write("R2: " + r2_score(df["y"], forecast["yhat"][:len(df["y"])]))
+        myfile.write("MSE: " + mean_squared_error(df["y"], forecast["yhat"][:len(df["y"])]))
+        myfile.write("MAE: " + mean_absolute_error(df["y"], forecast["yhat"][:len(df["y"])]))
+
+    with open('serialized_model' + str(all_params.index(params)) + '.json', 'w') as fout:
+        fout.write(model_to_json(m))  # Save model
+
+    # plot forecast
+    m.plot(forecast)
+    # save plot to image
+    pyplot.savefig("results/" + folder_name + "/forecast-" + str(all_params.index(params)) + ".png")
+
+    # plot forecast components
+    m.plot_components(forecast)
+    # save plot to image
+    pyplot.savefig("results/" + folder_name + "/forecast_components-" + str(all_params.index(params)) + ".png")
+
+    # plot changepoints
+    fig = m.plot(forecast)
+    a = add_changepoints_to_plot(fig.gca(), m, forecast)
+    # save plot to image
+    pyplot.savefig("results/" + folder_name + "/changepoints-" + str(all_params.index(params)) + ".png")
+
 # Find the best parameters
 tuning_results = pd.DataFrame(all_params)
 tuning_results['rmse'] = rmses
@@ -111,65 +192,8 @@ print(tuning_results)
 best_params = all_params[np.argmin(rmses)]
 print('Best params: ', best_params)
 
-#
-# # fit the model
-# model.fit(df)
-#
-# future = dgt(1200, "2020-01-01")
-# # save to csv
-# future.to_csv("data/future_gen.csv", index=False)
-#
-# future = model.make_future_dataframe(periods=200, freq='D')
-#
-# future.to_csv("data/future_gen_prophet.csv", index=False)
-#
-# print("future")
-# print(future.head())
-#
-# # use the model to make a forecast
-# forecast = model.predict(future)
-#
-# # diagnostics
-# df_cv = cross_validation(model, initial='730 days', period='180 days', horizon = '365 days')
-# df_p = performance_metrics(df_cv)
-#
-# print("df_cv")
-# print(df_cv.head())
-# print("df_p")
-# print(df_p.head())
-#
-#
-# # summarize the forecast
-# print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
-#
-# r2_score(df["y"], forecast["yhat"][:len(df["y"])])
-# mean_squared_error(df["y"], forecast["yhat"][:len(df["y"])])
-# mean_absolute_error(df["y"], forecast["yhat"][:len(df["y"])])
-#
-# print("Metrics")
-# print("R2: ", r2_score(df["y"], forecast["yhat"][:len(df["y"])]))
-# print("MSE: ", mean_squared_error(df["y"], forecast["yhat"][:len(df["y"])]))
-# print("MAE: ", mean_absolute_error(df["y"], forecast["yhat"][:len(df["y"])]))
-#
-#
-#
-# # plot forecast
-# model.plot(forecast)
-# pyplot.show()
-#
-# # plot forecast components
-# model.plot_components(forecast)
-# pyplot.show()
-#
-# from prophet.plot import add_changepoints_to_plot
-#
-# fig = model.plot(forecast)
-# a = add_changepoints_to_plot(fig.gca(), model, forecast)
-# pyplot.show()
-
-
-# with open('serialized_model.json', 'w') as fout:
-#     fout.write(model_to_json(m))  # Save model
-#
-# with open('serialized_model.json', 'r') as fin:
-#     m = model_from_json(fin.read())  # Load model
+with open("results/" + folder_name + "/results.txt", "a") as myfile:
+    myfile.write("\n\nFinal Result\n")
+    myfile.write(str(tuning_results))
+    myfile.write('Best params: ')
+    myfile.write(str(best_params))
